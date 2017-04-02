8f005515bafc0992d8b4ecb58106e3b237f882d2
Rajat-Goyal <rajat1881@gmail.com>
2017-04-03 01:08:17 +0530

    modular approach _/\_ dire

--- todo-server/controllers/todoController.js
+++ todo-server/controllers/todoController.js
@@ -1,49 +1,64 @@
 var Todo = require('../models/todo');
 
 
-exports.todo_list = function(req, res, next){
-  Todo.find( function(err, todos){
-    if(err) { res.send(err);}
+var getAllTodo = function(callback){
+  Todo.find( function(err, result){
+    if(err){
+      return callback(err, "error in getting list");
+    }
     else {
-      res.json(todos);
+      return callback(err, result);
     }
   });
+}
 
-};
-
-exports.todo_create = function(req, res, next){
+var createTodo = function(data, callback){
   Todo.create({
-    title : req.body.title,
-    text  : req.body.text,
-    done  : false
-  }, function(err, todo){
-    if(err) { res.send(err); }
-    else {
-      console.log("new item : " + req.body.title );
-      console.log(todo);
-      Todo.find( function(err, todos){
-        if(err) { res.send(err);}
-        else {
-          res.json(todos);
-        }
+    title: data.title,
+    text: data.text,
+    done: false
+  }, function(err, result){
+    if(err) {
+      return callback(err, "error in creating new entry");
+    }
+    else{
+      getAllTodo(function(err, list){
+        return callback(err, list);
       });
     }
   });
+};
+
+var deleteTodo = function(todoId, callback){
+  Todo.remove({_id: todoId})
+      .exec(function(err, result){
+        if(err){
+          return callback(err, "error in deleting item");
+        }
+        else{
+          getAllTodo(function(err, list){
+            return callback(err, list);
+          });
+        }
+      });
+};
+
+
+exports.todo_list = function(req, res, next){
+  getAllTodo( function(err, result) {
+    res.status(200).json(result);
+  });
+};
+
+exports.todo_create = function(req, res, next){
+  createTodo(req.body, function(err, newtodo){
+    res.status(200).json(newtodo);
+  })
 };
 
 exports.todo_delete = function(req, res, next){
   console.log("DELETE : " + req.params.todo_id);
-  Todo.remove({ _id: req.params.todo_id }, function(err, todo){
-    if(err) { res.send(err); }
-    else {
-      console.log("DELETED! : " + req.params.todo_id);
-      //this.todo_list();
-      Todo.find( function(err, todos){
-        if(err) { res.send(err);}
-        else {
-          res.json(todos);
-        }
-      });
-    }
-  })
+  deleteTodo(req.params.todo_id, function(err, newtodo){
+    res.status(200).json(newtodo);
+  });
 };
