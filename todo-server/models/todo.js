2243b2fd4e719b76f3d7b9a872fd85fda2de0c51
Rajat-Goyal <rajat1881@gmail.com>
2017-04-02 16:46:00 +0530

    minor nothing here

--- todo-server/models/todo.js
+++ todo-server/models/todo.js
@@ -5,7 +5,8 @@ var Schema = mongoose.Schema;
 var TodoSchema = Schema({
   title: {type: String, required: true},
   text: {type: String, required: true },
-  done: { type: Boolean, default: false}
+  done: {type: Boolean, default: false }
+//  type: { type: Boolean, required:true enum:['Login', 'Document', 'Number', 'true', 'false']}
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
--- todo-server/routes/api.js
+++ todo-server/routes/api.js
@@ -6,8 +6,8 @@ var router = express.Router();
 
 
 router.get('/', todo_controller.todo_list);                       // Get the list of TODO
-router.post('/create', todo_controller.todo_create);              //Create a new TODO item on POST
-router.delete('/delete/:todo_id', todo_controller.todo_delete);   //DELETE the TODO item
+router.post('/create', todo_controller.todo_create);              // Create a new TODO item on POST
+router.delete('/delete/:todo_id', todo_controller.todo_delete);   // DELETE the TODO item
 
 
 module.exports = router;
