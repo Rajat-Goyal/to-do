a58c5ae2172b02f1f7aabfe5c521c555511e07f0
Rajat-Goyal <rajat1881@gmail.com>
2017-04-09 16:32:46 +0530

    cleaning + modular

--- todo-server/public/apiService.js
+++ todo-server/public/apiService.js
@@ -6,7 +6,9 @@ apiService.$inject = ['$http' ];
 
 function apiService($http) {
     return {
-        getList: getList
+        getList: getList,
+        createItem: createItem,
+        deleteById: deleteById
     };
 
     function getList(){
@@ -15,12 +17,35 @@ function apiService($http) {
             .catch(getListFailed);
 
         function getListComplete(response) {
-            console.log("apiservice then called " + response); //  todo remove this
-            console.log(response.data); // todo remove this
             return response.data;
         }
         function getListFailed(error){
             console.log('Request failed for getList.' + error);
+        }
+    }
+    
+    function createItem(formData) {
+        return $http.post('/api/create', formData)
+            .then(createItemComplete)
+            .catch(createItemFailed);
+        function createItemComplete(response) {
+            return response.data;
+        }
+        function createItemFailed(error) {
+            console.log("Error creating new item " + error);
+        }
+    }
+
+    function deleteById(id){
+        return $http.delete('/api/delete/' + id)
+            .then(deleteComplete)
+            .catch(deleteFailed);
+
+        function deleteComplete(response) {
+            return response.data;
+        }
+        function deleteFailed(error) {
+            console.log('Error in deleting item ' + error) ;
         }
     }
 }
--- todo-server/public/core.js
+++ todo-server/public/core.js
@@ -2,14 +2,15 @@ angular
     .module('mytodo', [ 'ngMaterial', 'ngMdIcons' ])
     .controller('MainController', MainController);
 
-MainController.$inject = ['$http', 'apiService'];
+MainController.$inject = ['apiService'];
 
+function MainController( apiService ) {
+    var self = this;
 
-function MainController($http, apiService ) {
-    var my = this;
-
-    my.todos = {};
-    my.formData = {};
+    self.todos = {};
+    self.formData = {};
+    self.createTodo = createTodo;
+    self.deleteTodo = deleteTodo;
 
     activate();
 
@@ -22,46 +23,26 @@ function MainController($http, apiService ) {
     function getTodo(){
         return apiService.getList()
             .then(function (data) {
-                console.log("controller then called");
-                my.todos = data;
-                return my.todos;
-            })
+                self.todos = data;
+                return self.todos;
+            });
     }
 
-    // var self = this;
-    //
-    // self.openDialog = function($event){
-    //     $mdDialog.show({
-    //       controller: LoginCtrl,
-    //       controllerAs: 'ctrl',
-    //       templateUrl: 'login.dialog.html',
-    //       parent: angular.element(document.body),
-    //       targetEvent: $event,
-    //       clickOutsideToClose: true
-    //   })
-    // }
+    function createTodo(){
+        return apiService.createItem(self.formData)
+            .then(function(data){
+                self.todos = data;
+                self.formData = {};
+                return self.todos;
+            });
+    }
 
-    this.createTodo = function () {
-        console.log(my.formData);
-        $http.post('/api/create/', my.formData)
-            .success(function (data) {
-                my.formData = {};
-                my.todos = data;
-                console.log(data);
-            })
-            .error(function (data) {
-                console.log("Error " + data);
+    function deleteTodo( id ){
+        return apiService.deleteById(id)
+            .then(function(data){
+                self.todos = data;
+                return self.todos;
             });
-    };
-    this.deleteTodo = function (id) {
-        $http.delete('/api/delete/' + id)
-            .success(function (data) {
-                my.todos = data;
-            })
-            .error(function (data) {
-                console.log("Error" + data)
-            });
-    };
+    }
 }
 
- // }]);
