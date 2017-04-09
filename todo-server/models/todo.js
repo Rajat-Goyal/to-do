3a12910b1bf1dc26859825afdf4b4640f9bd5f9f
Rajat-Goyal <rajat1881@gmail.com>
2017-04-09 15:45:36 +0530

    add type field

--- todo-server/models/todo.js
+++ todo-server/models/todo.js
@@ -5,8 +5,8 @@ var Schema = mongoose.Schema;
 var TodoSchema = Schema({
   title: {type: String, required: true},
   text: {type: String, required: true },
-  done: {type: Boolean, default: false }
-//  type: { type: Boolean, required:true enum:['Login', 'Document', 'Number', 'true', 'false']}
+  done: {type: Boolean, default: false },
+  type: { type: String, required:true, enum:['Login', 'Document', 'Number', 'Bank'], default:'Document'}
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
